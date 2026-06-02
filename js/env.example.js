dd0d1cb80c8ab2d000aa32eb9c2845630575dca0
hikammin <hikami3150@gmail.com>
2026-06-02 17:52:12 +0800

    Delete js directory

--- js/env.example.js
+++
@@ -1,7 +0,0 @@
-// js/env.example.js
-// このファイルは env.js のテンプレートです。
-// 自身の環境に合わせてコピーして env.js を作成してください。
-window.ENV = {
-  ADMIN_EMAIL: 'your-admin-email@example.com',
-  ADMIN_PASS: 'your-secure-password'
-};
--- js/env.js
+++
@@ -1,4 +0,0 @@
-window.ENV = {
-  ADMIN_EMAIL: 'hikami3150@gmail.com',
-  ADMIN_PASS: 'Kaikai315'
-};
